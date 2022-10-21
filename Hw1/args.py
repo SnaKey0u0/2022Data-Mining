fc3dd2f458f9d984335945ca98b48883bf031a01
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2022-10-21 16:12:04 +0800

    seriously final update

--- Hw1/args.py
+++ Hw1/args.py
@@ -7,8 +7,8 @@ def parse_args():
     def a(*args, **kwargs):
         p.add_argument(*args, **kwargs)
 
-    a('--min_sup', type=float, default=500, help='Minimum support')
-    a('--min_conf', type=float, default=0.01, help='Minimum confidence')
+    a('--min_sup', type=float, default=1200, help='Minimum support')
+    a('--min_conf', type=float, default=0.7, help='Minimum confidence')
     a('--dataset', type=str, default='2022-DM-release-testdata-2.data', help='Dataset to use, please include the extension')
 
     return p.parse_args()
